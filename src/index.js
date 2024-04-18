be6ddb921f6d09cb39ea39af9ef1d468d5b422a4
michel <jsnerdic@gmail.com>
2024-04-18 15:19:31 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import GridExcelExport from './GridExcelExport';
+
+function App() {
+  return (
+    <div>
+      <GridExcelExport />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
